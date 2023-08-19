7d48384ddab5b10cfc6b192f771806c7675ace85
Phou Kiethseng <puseng123@gmail.com>
2023-08-19 19:49:36 +0800

    Make small changes to contact form schema

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -3,9 +3,13 @@ import { z } from "zod";
 const ContactFormContentCharLimit = 1000;
 
 const contactFormSchema = z.object({
-  name: z.string().nonempty().min(1).max(50),
-  email: z.string().min(1).email({ message: "Invalid Email Address" }).max(320),
-  message: z.string().max(ContactFormContentCharLimit),
+  name: z.string().nonempty().nonempty().max(50),
+  email: z
+    .string()
+    .nonempty()
+    .email({ message: "Invalid Email Address" })
+    .max(320),
+  message: z.string().max(ContactFormContentCharLimit).nonempty(),
 });
 
 export const zodSchemas = {
