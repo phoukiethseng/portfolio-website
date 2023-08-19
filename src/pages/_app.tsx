224b7ec95bcafeb0098d37e8692140436d6aa31c
Phou Kiethseng <puseng123@gmail.com>
2023-08-19 15:05:24 +0800

    Added HotJar for web analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import { type AppType } from "next/dist/shared/lib/utils";
 import "@/styles/globals.css";
 import ContextProviders from "@/components/context-providers";
 import Layout from "@/components/layout";
+import Script from "next/script";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
+      <Script src="/hotjar.js" />
       <ContextProviders>
         <Layout>
           <Component {...pageProps} />
