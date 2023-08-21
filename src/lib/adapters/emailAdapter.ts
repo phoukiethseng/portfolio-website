a8b8463cf15c92e9f77fefe1e3bf6fdaccbee92c
Phou Kiethseng <puseng123@gmail.com>
2023-08-22 01:53:27 +0800

    Properly handle emailJs api response

--- src/lib/adapters/emailAdapter.ts
+++ src/lib/adapters/emailAdapter.ts
@@ -4,6 +4,7 @@ import { EmailService } from "../services/email";
 import { DataMapper, StatusResponse } from "../types";
 import { EmailMessage, EmailJsTemplateParams } from "../validationSchemas";
 import { env } from "@/env.mjs";
+import { EmailJSResponseStatus } from "@emailjs/browser";
 
 export function useEmailJS(): EmailService<
   EmailMessage,
@@ -14,11 +15,16 @@ export function useEmailJS(): EmailService<
   const emailClient = emailjs;
   async function send(emailMessage: EmailMessage): Promise<StatusResponse> {
     try {
-      await emailClient.send(
+      const response: EmailJSResponseStatus = await emailClient.send(
         env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
         env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
         map(emailMessage)
       );
+      if (response.status !== 200) {
+        return {
+          status: "failed",
+        };
+      }
     } catch (error) {
       return {
         status: "failed",
