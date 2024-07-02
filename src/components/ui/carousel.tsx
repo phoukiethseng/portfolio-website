2b9243f6081c6274d39cba33ef0f691a0ec4fd8e
Phou Kiethseng <puseng123@gmail.com>
2024-07-02 20:00:58 +0700

    Bump workflow NodeJS to version 20

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -204,7 +204,7 @@ const CarouselPrevious = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute  h-8 w-8 rounded-full",
+        "group  absolute h-8 w-8 rounded-full",
         orientation === "horizontal"
           ? "-left-12 top-1/2 -translate-y-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -214,7 +214,7 @@ const CarouselPrevious = React.forwardRef<
       onClick={scrollPrev}
       {...props}
     >
-      <ArrowLeft className="h-4 w-4" />
+      <ArrowLeft className="h-4 w-4 group-hover:text-white" />
       <span className="sr-only">Previous slide</span>
     </Button>
   );
